5eb1b4829af9e3ad6b0f1504556443c28ea99cd0
Keewon Seo <keewon.seo@tapjoy.com>
2016-12-28 15:14:43 +0900

    default loop count should be 1

--- FLAnimatedImage/FLAnimatedImage.m
+++ FLAnimatedImage/FLAnimatedImage.m
@@ -228,7 +228,12 @@ static NSHashTable *allAnimatedImagesWeak;
         //     };
         // }
         NSDictionary *imageProperties = (__bridge_transfer NSDictionary *)CGImageSourceCopyProperties(_imageSource, NULL);
-        _loopCount = [[[imageProperties objectForKey:(id)kCGImagePropertyGIFDictionary] objectForKey:(id)kCGImagePropertyGIFLoopCount] unsignedIntegerValue];
+        id loopCountObj = [[imageProperties objectForKey:(id)kCGImagePropertyGIFDictionary] objectForKey:(id)kCGImagePropertyGIFLoopCount];
+        if ([loopCountObj respondsToSelector:@selector(unsignedIntegerValue)]) {
+            _loopCount = [loopCountObj unsignedIntegerValue];
+        } else {
+            _loopCount = 1;
+        }
         
         // Iterate through frame images
         size_t imageCount = CGImageSourceGetCount(_imageSource);
