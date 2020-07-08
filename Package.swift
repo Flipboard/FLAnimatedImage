eefdfabd5ac7d7b6483d50566e27a3965a24c5de
Dal Rupnik <legoless@gmail.com>
2020-07-08 15:26:07 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "FLAnimatedImage",
+    platforms: [
+        .iOS(.v8)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -18,7 +21,7 @@ let package = Package(
             name: "FLAnimatedImage",
             path: "FLAnimatedImage",
             exclude: [ "Info.plist" ],
-            sources: [ "./FLAnimatedImage" ],
+            sources: [ "FLAnimatedImageView.m", "FLAnimatedImage.m" ],
             publicHeadersPath: "./FLAnimatedImage"
         )
     ]
